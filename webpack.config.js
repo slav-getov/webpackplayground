720cf3a49ec7678c40efefa17842eca6f7daa3dc
Preslav Getov <preslav.getov@yara.com>
2023-05-10 11:30:22 +0300

    adding bundle analyzer so some basic info is desplayed

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BundleAnalyzerPlugin =
+  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 module.exports = {
   mode: "development",
   entry: path.resolve(__dirname, "src/index.js"),
@@ -37,5 +39,6 @@ module.exports = {
       filename: "index.html",
       template: "src/template.html",
     }),
+    new BundleAnalyzerPlugin(),
   ],
 };
